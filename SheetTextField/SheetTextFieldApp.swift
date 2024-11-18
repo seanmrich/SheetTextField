09cd6e91be972509a7ae6fc752e89ff094202adf
Sean <sean@snowfort.software>
2024-11-18 16:28:41 -0500

    Initial Commit

---
+++ SheetTextField/SheetTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetTextFieldApp.swift
+//  SheetTextField
+//
+//  Created by Sean Rich on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SheetTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
